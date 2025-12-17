905915672f236eabb937196caa54ba00b00d8f09
Vincenza <vinni2690@hotmail.com>
2025-12-17 11:57:57 +0100

    controllers

---
+++ src/main/java/it/moonril/family_recipes_backend/controller/IngredientController.java
@@ -0,0 +1,9 @@
+package it.moonril.family_recipes_backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "ingredients")
+public class IngredientController {
+}
