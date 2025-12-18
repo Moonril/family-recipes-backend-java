0d5477a5ea1ab7adbcf42d45996622be2b8a28b9
Vincenza <vinni2690@hotmail.com>
2025-12-18 17:25:18 +0100

    recipe service complete

--- src/main/java/it/moonril/family_recipes_backend/repository/RecipeRepository.java
+++ src/main/java/it/moonril/family_recipes_backend/repository/RecipeRepository.java
@@ -2,10 +2,15 @@ package it.moonril.family_recipes_backend.repository;
 
 import it.moonril.family_recipes_backend.models.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Integer>, PagingAndSortingRepository<Recipe, Integer> {
     Optional<Recipe> findByTitle(String name);
+    @Query("SELECT r FROM Recipe r WHERE LOWER(r.title) LIKE LOWER(CONCAT('%', :title, '%'))")
+    List<Recipe> findByTitleContainingIgnoreCase(@Param("title") String name);
 }
--- src/main/java/it/moonril/family_recipes_backend/service/RecipeService.java
+++ src/main/java/it/moonril/family_recipes_backend/service/RecipeService.java
@@ -6,8 +6,13 @@ import it.moonril.family_recipes_backend.exceptions.NotFoundException;
 import it.moonril.family_recipes_backend.models.Recipe;
 import it.moonril.family_recipes_backend.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -30,4 +35,41 @@ public class RecipeService {
 
         return recipeRepository.save(recipe);
     }
+
+    public Page<Recipe> getAllRecipes(int page, int size, String sortBy){
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return recipeRepository.findAll(pageable);
+    }
+
+    public Recipe getRecipe(int id) throws NotFoundException{
+        return recipeRepository.findById(id).orElseThrow(() -> new NotFoundException("Recipe with id: " + id + " not found"));
+    }
+
+    public List<Recipe> getRecipesByTitle(String title) throws NotFoundException{
+        List<Recipe> recipes = recipeRepository.findByTitleContainingIgnoreCase(title);
+
+        if (recipes.isEmpty()) {
+            throw new NotFoundException("Nessuna ricetta trovata con questo nome: " + title);
+        }
+
+        return recipes;
+    }
+
+    public Recipe updateRecipe(int id, RecipeDto recipeDto) throws NotFoundException{
+        Recipe recipeToUpdate = getRecipe(id);
+
+        recipeToUpdate.setImage(recipeDto.getImage());
+        recipeToUpdate.setTitle(recipeDto.getTitle());
+        recipeToUpdate.setDescription(recipeDto.getDescription());
+        recipeToUpdate.setRecipeType(recipeDto.getRecipeType());
+
+        return recipeRepository.save(recipeToUpdate);
+    }
+
+    public void deleteRecipe(int id) throws NotFoundException{
+        Recipe recipeToDelete = getRecipe(id);
+
+        recipeRepository.delete(recipeToDelete);
+    }
+
 }
