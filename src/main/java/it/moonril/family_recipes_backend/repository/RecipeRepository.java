a38f805554bfc802afb6b3c262457d551ef18f7d
Vincenza <vinni2690@hotmail.com>
2025-12-18 17:13:16 +0100

    .

--- src/main/java/it/moonril/family_recipes_backend/repository/RecipeRepository.java
+++ src/main/java/it/moonril/family_recipes_backend/repository/RecipeRepository.java
@@ -4,5 +4,8 @@ import it.moonril.family_recipes_backend.models.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
+
 public interface RecipeRepository extends JpaRepository<Recipe, Integer>, PagingAndSortingRepository<Recipe, Integer> {
+    Optional<Recipe> findByTitle(String name);
 }
--- src/main/java/it/moonril/family_recipes_backend/service/RecipeService.java
+++ src/main/java/it/moonril/family_recipes_backend/service/RecipeService.java
@@ -1,7 +1,33 @@
 package it.moonril.family_recipes_backend.service;
 
+import it.moonril.family_recipes_backend.dto.RecipeDto;
+import it.moonril.family_recipes_backend.exceptions.EmailAlreadyExistsException;
+import it.moonril.family_recipes_backend.exceptions.NotFoundException;
+import it.moonril.family_recipes_backend.models.Recipe;
+import it.moonril.family_recipes_backend.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RecipeService {
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    public Recipe saveRecipe(RecipeDto recipeDto) throws NotFoundException {
+        Optional<Recipe> existingRecipe = recipeRepository.findByTitle(recipeDto.getTitle());
+
+        if (existingRecipe.isPresent()) {
+            throw new EmailAlreadyExistsException(recipeDto.getTitle());
+        }
+        Recipe recipe = new Recipe();
+
+        recipe.setImage(recipeDto.getImage());
+        recipe.setTitle(recipeDto.getTitle());
+        recipe.setDescription(recipeDto.getDescription());
+        recipe.setRecipeType(recipeDto.getRecipeType());
+
+        return recipeRepository.save(recipe);
+    }
 }
